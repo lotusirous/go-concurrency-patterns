8ff7dc22bb6415bbc7bc00a774592093cf3811e3
lotusirous <nguyentrongkha92@gmail.com>
2021-03-14 12:55:39 +0900

    add manage goroutine

--- 18-worker-pool/main.go
+++ 18-worker-pool/main.go
@@ -1,10 +1,22 @@
 // Credit:
 // https://gobyexample.com/worker-pools
 
+// Worker pool benefits:
+// - Efficiency because it distributes the work across threads.
+// - Flow control: Limit work in flight
+
+// Disadvantage of worker:
+// Lifetimes complexity: clean up and idle worker
+
+// Principles:
+// Start goroutines whenever you have the concurrent work to do.
+// The goroutine should exit as soon as posible the work is done. This helps us
+// to clean up the resources and manage the lifetimes correctly.
 package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -17,17 +29,47 @@ func worker(id int, jobs <-chan int, results chan<- int) {
 	}
 }
 
+func workerEfficient(id int, jobs <-chan int, results chan<- int) {
+	// sync.WaitGroup helps us to manage the job
+	var wg sync.WaitGroup
+	for j := range jobs {
+
+		wg.Add(1)
+		// we start a goroutine to run the job
+		go func(job int) {
+			// start the job
+			fmt.Println("worker", id, "started job", job)
+			time.Sleep(time.Second)
+			fmt.Println("worker", id, "fnished job", job)
+			results <- job * 2
+			wg.Done()
+
+		}(j)
+
+	}
+	// With a help to manage the lifetimes of goroutines
+	// we can add more handler when a goroutine finished
+	wg.Wait()
+}
 func main() {
 	const numbJobs = 8
 	jobs := make(chan int, numbJobs)
 	results := make(chan int, numbJobs)
 
-	// start a group of workers
+	// 1. Start the worker
+	// it is a fixed pool of goroutines receive and perform tasks from a channel
+
+	// In this example, we define a fixed 3 workers
+	// they receive the `jobs` from the channel jobs
+	// we also naming the worker name with `w` variable.
 	for w := 1; w <= 3; w++ {
-		go worker(w, jobs, results)
+		go workerEfficient(w, jobs, results)
 	}
 
-	// send data to worker group
+	// 2. send the work
+	// other goroutine sends the work to the channels
+
+	// in this example, the `main` goroutine sends the work to the channel `jobs`
 	for j := 1; j <= numbJobs; j++ {
 		jobs <- j
 	}
--- README.md
+++ README.md
@@ -1,34 +1,12 @@
 # Go Concurrency Patterns
 
-This repository aims to collect pretty patterns for concurrency in Golang
+This repository collects common concurrency patterns in Golang
 
 
+## Materials
+- [Go Concurrency Patterns](https://talks.golang.org/2012/concurrency.slide#1)
+and [source](https://talks.golang.org/2012/concurrency/support/)
 
-## Basic
+- [Advanced Go Concurrency Patterns](https://talks.golang.org/2013/advconc.slide)
+- [Rethinking classical concurrency pattern](https://www.youtube.com/watch?v=5zXAHh5tJqQ)
 
-- Slide: https://talks.golang.org/2012/concurrency.slide#1
-
-- Code: https://talks.golang.org/2012/concurrency/support/
-
-## Advance
-
-- Slide: https://talks.golang.org/2013/advconc.slide
-
-## (2018) Rethinking classical concurrency pattern
-
-- https://www.youtube.com/watch?v=5zXAHh5tJqQ
-
-Principles:
-
-1. Start goroutine when you have concurrent work.
-2. Share by communication.
-
-
-- Asynchronous: **returns** to the caller **before its result is ready**.
-```go
-func Fetch(name string) <-chan Item {
-    c := make(chan Item, 1)
-}
-```
-- Condition variables
-- Worker pools
