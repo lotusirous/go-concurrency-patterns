cbc3f7e1e3820f96de975af1bacf7004111b9897
lotusirous <nguyentrongkha92@gmail.com>
2021-03-15 22:11:19 +0900

    Add generator explanation

--- 3-generator/main.go
+++ 3-generator/main.go
@@ -6,14 +6,21 @@ import (
 	"time"
 )
 
-// the boring function return a channel to communicate with it.
-func boring(msg string) <-chan string { // <-chan string means receives-only channel of string.
+// boring is a function that returns a channel to communicate with it.
+// <-chan string means receives-only channel of string.
+func boring(msg string) <-chan string {
 	c := make(chan string)
-	go func() { // we launch goroutine inside a function.
-		for i := 0; ; i++ {
+	// we launch goroutine inside a function
+	// that sends the data to channel
+	go func() {
+		// The for loop simulate the infinite sender.
+		for i := 0; i < 10; i++ {
 			c <- fmt.Sprintf("%s %d", msg, i)
 			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 		}
+
+		// The sender should close the channel
+		close(c)
 
 	}()
 	return c // return a channel to caller.
@@ -21,15 +28,19 @@ func boring(msg string) <-chan string { // <-chan string means receives-only cha
 
 func main() {
 
-	// Now, we don't need a this channel anymore
-	// c := make(chan string)
-	joe := boring("Joe") // function returns a channel
+	joe := boring("Joe")
 	ahn := boring("Ahn")
 
-	for i := 0; i < 5; i++ {
+	// This loop yields 2 channels in sequence
+	for i := 0; i < 10; i++ {
 		fmt.Println(<-joe)
 		fmt.Println(<-ahn)
 	}
+
+	// or we can simply use the for range
+	// for msg := range joe {
+	// 	fmt.Println(msg)
+	// }
 	fmt.Println("You're both boring. I'm leaving")
 
 }
